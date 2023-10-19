9485db1b9a16f98d31d1c29dce05e6ac48378ecf
ALayC <97717364+ALayC@users.noreply.github.com>
2023-10-19 01:06:39 -0300

    19-10 crear reserva

--- src/clases/Medico.java
+++ src/clases/Medico.java
@@ -2,20 +2,69 @@ package clases;
 
 import tads.ListaN;
 import tads.*;
+import java.time.LocalDate;
 
 public class Medico implements Comparable<Medico> {
+
+    /**
+     * @return the listaConsultas
+     */
+    public ListaN getListaConsultas() {
+        return listaConsultas;
+    }
+
+    /**
+     * @param listaConsultas the listaConsultas to set
+     */
+    public void setListaConsultas(ListaN listaConsultas) {
+        this.listaConsultas = listaConsultas;
+    }
 
     private String nombre;
     private int codMedico;
     private int tel;
     private int especialidad;
     private ListaN listaConsultas;
+    private Cola<Paciente> listaDeEspera;
 
     public Medico(String elNombre, int elCod, int elTel, int laEspecialida) {
         this.setNombre(elNombre);
         this.setCodMedico(elCod);
         this.setTel(elTel);
         this.setEspecialidad(laEspecialida);
+        this.listaDeEspera = new Cola<>();
+        this.listaConsultas = new ListaN();
+    }
+
+    public int cantElementosParaFecha(LocalDate fecha) {
+        int contador = 0;
+        for (int i = 0; i < getListaConsultas().cantElementos(); i++) {
+            Reserva reserva = (Reserva) getListaConsultas().obtenerElemento(i);
+
+            if (reserva.getFecha().equals(fecha)) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public boolean tieneConsultaConPacienteEnFecha(int ciPaciente, LocalDate fecha) {
+        for (int i = 0; i < getListaConsultas().cantElementos(); i++) {
+            Reserva reserva = (Reserva) getListaConsultas().obtenerElemento(i);
+
+            if (reserva.getFecha().equals(fecha) && reserva.getCiPaciente() == ciPaciente) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Cola<Paciente> getListaDeEspera() {
+        return listaDeEspera;
+    }
+
+    public void setListaDeEspera(Cola<Paciente> listaDeEspera) {
+        this.listaDeEspera = listaDeEspera;
     }
 
     /**
@@ -73,16 +122,14 @@ public class Medico implements Comparable<Medico> {
     public void setEspecialidad(int especialidad) {
         this.especialidad = especialidad;
     }
-    
-    
-    
+
     @Override
     public boolean equals(Object obj) {
         Medico md = (Medico) obj;
         boolean es = false;
 
         if (this.codMedico == md.codMedico) {
-            es=true;
+            es = true;
             return es;
         }
         return es;
@@ -90,13 +137,13 @@ public class Medico implements Comparable<Medico> {
 
     @Override
     public int compareTo(Medico o) {
-   // return Integer.compare(this.codMedico, o.codMedico);
-    return this.getNombre().compareTo(o.getNombre());
+        // return Integer.compare(this.codMedico, o.codMedico);
+        return this.getNombre().compareTo(o.getNombre());
 
     }
-    
+
     @Override
-   public String toString() {
+    public String toString() {
         return "Medico: " + nombre + "\nCodigo: " + codMedico + "\nEspecialidad: " + especialidad + "\n";
     }
 }
