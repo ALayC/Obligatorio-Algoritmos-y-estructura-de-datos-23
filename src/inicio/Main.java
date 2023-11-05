cafcf0c90ddac105c1442793dc7707be1906e1cb
ALayC <97717364+ALayC@users.noreply.github.com>
2023-11-05 19:51:27 -0300

    19-10 metodo crear reserva finalizado

--- src/inicio/Main.java
+++ src/inicio/Main.java
@@ -15,33 +15,30 @@ public class Main {
 
         p1_crearSistema(p, s);
         p2_registrarMedico(p, s);
-        p4_registrarPaciente(p, s);
         p21_diaDeConsultaMedico(p, s);
+        p2_3_eliminarMedico(p, s);
+        p4_registrarPaciente(p, s);
+        p2_5_eliminarPaciente(p, s);
         p2_6_crearReserva(p, s);
-        p3_5consultasPendientesPaciente(p, s);
-        p3_3ListarConsultas(p, s);
-
+        p2_7_cancelarReserva(p, s);
         p2_8_anunciaLlegada(p, s);
         p2_9_terminarConsultaMedicoPaciente(p, s);
-        //p2_10_cerrarConsulta(p, s);
-        //p3_6historiaClínicaPaciente(p, s);
-        //p3_4ListarPacientesEnEspera(p, s);
-        //p2_7_cancelarReserva(p, s);
-        //p3_4ListarPacientesEnEspera(p, s);
-        //p3_1ListaMedicos(p, s);
-        //p2_3_eliminarMedico(p, s);
-        //p3_1ListaMedicos(p, s);
-        //p3_2ListarPacientes(p, s);
-        //p2_5_eliminarPaciente(p,s);
-        //p3_2ListarPacientes(p, s);
+        p2_10_cerrarConsulta(p, s);
+        p3_1ListaMedicos(p, s);
+        p3_2ListarPacientes(p, s);
+        p3_3ListarConsultas(p, s);
+        p3_4ListarPacientesEnEspera(p, s);
+        p3_5consultasPendientesPaciente(p, s);
+        p3_6historiaClínicaPaciente(p, s);
         p3_7reporteDePacientesXFechaYEspecialidad(p, s);
+
         p.imprimirResultadosPrueba();
     }
 
     public static void p1_crearSistema(Prueba p, Sistema s) {
 
         p.ver(s.crearSistemaDeAutogestion(10).resultado, Retorno.Resultado.OK, "Creacion correcta del sistema");
-        //p.ver(s.crearSistemaDeAutogestion(-1).resultado, Retorno.Resultado.ERROR_1, "No se crea el sistema,cantidad no valida");
+        p.ver(s.crearSistemaDeAutogestion(-1).resultado, Retorno.Resultado.ERROR_1, "No se crea el sistema,cantidad no valida");
 
     }
 
@@ -49,9 +46,10 @@ public class Main {
         p.ver(s.registrarMedico("Daniel", 1, 12345678, 3).resultado, Retorno.Resultado.OK, "Se registra el medico");
         p.ver(s.registrarMedico("Lucia", 2, 87654321, 10).resultado, Retorno.Resultado.OK, "Se registra el medico");
         p.ver(s.registrarMedico("Daniela", 3, 14785236, 13).resultado, Retorno.Resultado.OK, "Se registra el medico");
+        p.ver(s.registrarMedico("ELIMINAR", 4, 00000000, 14).resultado, Retorno.Resultado.OK, "Se registra el medico");
 
-        //p.ver(s.registrarMedico("Andres", 1, 98765432, 4).resultado, Retorno.Resultado.ERROR_1, "No se registra, medico con igual codigo");
-        // p.ver(s.registrarMedico("Lucas", 4, 65498732, 22).resultado, Retorno.Resultado.ERROR_2, "No se registra, medico con especialidad no valida");
+        p.ver(s.registrarMedico("Andres", 1, 98765432, 4).resultado, Retorno.Resultado.ERROR_1, "No se registra, medico con igual codigo");
+        p.ver(s.registrarMedico("Lucas", 5, 65498732, 22).resultado, Retorno.Resultado.ERROR_2, "No se registra, medico con especialidad no valida");
     }
 
     public static void p21_diaDeConsultaMedico(Prueba p, Sistema s) {
@@ -78,19 +76,20 @@ public class Main {
         p.ver(s.agregarPaciente("Martin", 78901234, "calle 9").resultado, Retorno.Resultado.OK, "Se registra el paciente");
         p.ver(s.agregarPaciente("Isabel", 89012345, "calle 10").resultado, Retorno.Resultado.OK, "Se registra el paciente");
         p.ver(s.agregarPaciente("Roberto", 90123456, "calle 11").resultado, Retorno.Resultado.OK, "Se registra el paciente");
+        p.ver(s.agregarPaciente("ELIMINAR", 45678912, "calle 13").resultado, Retorno.Resultado.OK, "Se registra el paciente");
 
     }
 
     public static void p2_3_eliminarMedico(Prueba p, Sistema s) {
-        //p.ver(s.eliminarMedico(1).resultado, Retorno.Resultado.ERROR_1, "Este medico no existe");
-        p.ver(s.eliminarMedico(1).resultado, Retorno.Resultado.OK, "Se elimina el medico");
-        //p.ver(s.eliminarMedico(2).resultado, Retorno.Resultado.ERROR_1, "Este medico no existe");
+        p.ver(s.eliminarMedico(300).resultado, Retorno.Resultado.ERROR_1, "Este medico no existe");
+        p.ver(s.eliminarMedico(4).resultado, Retorno.Resultado.OK, "Se elimina el medico");
+        p.ver(s.eliminarMedico(999).resultado, Retorno.Resultado.ERROR_1, "Este medico no existe");
     }
 
     public static void p2_5_eliminarPaciente(Prueba p, Sistema s) {
         p.ver(s.eliminarPaciente(100).resultado, Retorno.Resultado.ERROR_1, "Este paciente no existe");
-        p.ver(s.eliminarPaciente(12345678).resultado, Retorno.Resultado.OK, "Se elimina paciente");
-        p.ver(s.eliminarPaciente(12345678).resultado, Retorno.Resultado.ERROR_1, "Este paciente no existe");
+        p.ver(s.eliminarPaciente(45678912).resultado, Retorno.Resultado.OK, "Se elimina paciente");
+        p.ver(s.eliminarPaciente(444444).resultado, Retorno.Resultado.ERROR_1, "Este paciente no existe");
 
     }
 
@@ -103,13 +102,13 @@ public class Main {
         //p.ver(s.reservaConsulta(1, 12345678, fecha2).resultado, Retorno.Resultado.OK, "Crea la reserva para German");
         p.ver(s.reservaConsulta(1, 14523689, fecha).resultado, Retorno.Resultado.OK, "Crea la reserva para Romina");
         p.ver(s.reservaConsulta(1, 32165478, fecha).resultado, Retorno.Resultado.OK, "Crea la reserva para Jose");
-        /* p.ver(s.reservaConsulta(1, 23456789, fecha).resultado, Retorno.Resultado.OK, "Crea la reserva para Maria");
+        p.ver(s.reservaConsulta(1, 23456789, fecha).resultado, Retorno.Resultado.OK, "Crea la reserva para Maria");
         p.ver(s.reservaConsulta(1, 34567890, fecha).resultado, Retorno.Resultado.OK, "Crea la reserva para Lucas");
         p.ver(s.reservaConsulta(1, 45678901, fecha).resultado, Retorno.Resultado.OK, "Crea la reserva para Daniela");
         p.ver(s.reservaConsulta(1, 56789012, fecha).resultado, Retorno.Resultado.OK, "Crea la reserva para Andres");
         p.ver(s.reservaConsulta(1, 67890123, fecha).resultado, Retorno.Resultado.OK, "Crea la reserva para Carolina");
         p.ver(s.reservaConsulta(1, 78901234, fecha).resultado, Retorno.Resultado.OK, "Crea la reserva para Martin");
-        p.ver(s.reservaConsulta(1, 89012345, fecha).resultado, Retorno.Resultado.OK, "Crea la reserva para Isabel");*/
+        p.ver(s.reservaConsulta(1, 89012345, fecha).resultado, Retorno.Resultado.OK, "Crea la reserva para Isabel");
 
         p.ver(s.reservaConsulta(2, 12345678, fecha2).resultado, Retorno.Resultado.OK, "Crea la reserva para German");
         p.ver(s.reservaConsulta(2, 14523689, fecha2).resultado, Retorno.Resultado.OK, "Crea la reserva para Romina");
@@ -161,7 +160,11 @@ public class Main {
         p.ver(s.terminarConsultaMedicoPaciente(12345678, 3, "Dolor de panza").resultado, Retorno.Resultado.OK, "Se cierra la consulta y se agrega al historial medico");
         p.ver(s.terminarConsultaMedicoPaciente(14523689, 3, "Dolor de panza").resultado, Retorno.Resultado.OK, "Se cierra la consulta y se agrega al historial medico");
         p.ver(s.terminarConsultaMedicoPaciente(32165478, 3, "Dolor de panza").resultado, Retorno.Resultado.OK, "Se cierra la consulta y se agrega al historial medico");
-        //p.ver(s.terminarConsultaMedicoPaciente(32165478, 2, "Dolor de panza").resultado, Retorno.Resultado.OK, "Se cierra la consulta y se agrega al historial medico");
+
+        p.ver(s.terminarConsultaMedicoPaciente(99999999, 3, "Dolor de panza").resultado, Retorno.Resultado.ERROR_1, "Esa cedula no existe");
+        
+        
+
     }
 
     public static void p2_10_cerrarConsulta(Prueba p, Sistema s) {
@@ -170,9 +173,8 @@ public class Main {
 
         LocalDate fecha2 = LocalDate.parse("16/10/2023", formatter);
 
-        // LocalDate fecha2 = LocalDate.parse("16/10/2023", formatter);
         p.ver(s.cerrarConsulta(1, fecha).resultado, Retorno.Resultado.OK, "Paciente no asiste a la consulta, se deja registro en el historial medico");
-        //p.ver(s.cerrarConsulta(1, fecha2).resultado, Retorno.Resultado.OK, "Paciente no asiste a la consulta, se deja registro en el historial medico");
+
     }
 
     public static void p3_1ListaMedicos(Prueba p, Sistema s) {
