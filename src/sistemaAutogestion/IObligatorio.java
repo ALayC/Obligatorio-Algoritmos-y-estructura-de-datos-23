79679a5a9e98ab1e6c5a0b9e7a464e04627e5daa
ALayC <97717364+ALayC@users.noreply.github.com>
2023-11-05 17:24:07 -0300

    19-10 metodo crear reserva finalizado

--- src/sistemaAutogestion/IObligatorio.java
+++ src/sistemaAutogestion/IObligatorio.java
@@ -10,30 +10,30 @@ public interface IObligatorio {
     **************** REGISTROS **************************************
     */
     
-    //pre: que max sea valido (entre 0 y el tope)    post: se crea sistema correctamente
+    //pre: Se espera recibir por parametro un entero, mayor que 0 y menor que 15  post: Se crea sistema correctamente
     public Retorno crearSistemaDeAutogestion(int maxPacientesporMedico);
-     //pre:  no existencia de un medico con el mismo codMedico, especialidad valida      post:registro correcto del medico
+     //pre:Se espera recibir por parametro un string no vacio,un entero mayor que 0 para el "codMedico",un entero para "tel" y un entero mayor que 0 y menor que 15 para "especialidad"   post:registro correcto del medico
     public Retorno registrarMedico(String nombre,int codMedico,int tel, int especialidad); 
-     //pre: existe un medico con ese codigo. El medico no podrá consultas en espera      post: se elimina el medico con el codigo indicado
+     //pre:Se recibe por parametro un entero,que ya debe estar registrado como medico y no debe tenerninguna consulta agendada post: se elimina el medico con el codigo indicado
     public  Retorno eliminarMedico(int codMedico); 
-     //pre: no existe un paciente con esa CI      post: se registra correctamente el paciente en el sistema
+     //pre: Se recibe por parametro un string no vacio para "nombre" y "direccion", tambien se recibe un entero para "CI"  post: se registra correctamente el paciente en el sistema
     public Retorno agregarPaciente(String nombre, int CI, String direccion); 
-     //pre: debe estar registrada la CI en el sistema. El paciente no debe tener ninguna consulta agendada       post: elimina el paciente del sistema
+     //pre: Se recibe por parametro un entero que debe estar registrado previamente post: elimina el paciente del sistema
     public Retorno eliminarPaciente(int CI); 
 
      /*
     **************** GESTIÓN DE CONSULTAS **************************************
     */
       
-    //pre:      post:
+    //pre:Se recibe por parametro un entero para "codMedico" y "ciPaciente" que deben estar previamente registrados, tambien recibe una fecha  post:Se crea la reserva para el paciente, con el medico y la fecha recibida por parametro
     public Retorno reservaConsulta(int codMedico, int ciPaciente, LocalDate fecha);
-    //pre:      post:
+    //pre: Se recibe por parametro un entero un entero para "codMedico" y  "ciPaciente" que deben estar previamente registrados   post:Se cancela la reserva del paciente con el medico recibido por parametro
     public Retorno cancelarReserva(int codMedico, int ciPaciente); 
-    //pre:      post:
+    //pre: Se recibe por parametro un entero un entero para "codMedico" y  "ciPaciente" que deben estar previamente registrados  post:Se cambia el estado de la reserva que pasa a estado: "EN_ESPERA"
     public Retorno anunciaLlegada(int codMedico, int CIPaciente); 
-    //pre:      post:
+    //pre:  Se recibe por parametro un entero un entero para "codMedico" y  "ciPaciente" que deben estar previamente registrados, tambien recive un string no vacio para "detalleDeConsulta"    post:Se cierra la consulta y pasa a estado: "TERMINADA"
     public Retorno terminarConsultaMedicoPaciente(int CIPaciente, int codMedico, String detalleDeConsulta);   
-    //pre:      post:
+    //pre: Se recibe por aprametro un entero y una fecha    post:Se cierra la consulta y pasa a estad : "NO_ASISTIO"
     public Retorno cerrarConsulta(int codMedico, LocalDate fechaConsulta); 
     
  
@@ -41,19 +41,19 @@ public interface IObligatorio {
     **************** LISTADO Y REPORTES **************************************
     */
     
-    //pre:       post: se listan los medicos ordenados alfabeticamente
+    //pre:       post: Se listan los medicos ordenados alfabeticamente
     public Retorno listarMédicos(); 
-    //pre:      post: listar paciente en orden de registro
+    //pre:      post: Se listan los  pacientes en orden de registro
     public Retorno listarPacientes();     
-    //pre:      post:
+    //pre:      post:Se listan las consultas para el medico que recibe por aprametro ordenadas por dia
     public Retorno listarConsultas(int codMedico); 
-    //pre:      post:
+    //pre:      post:Se listan todos los pacientes en espera(estado “en espera”) para el medico recibido por parametro 
     public Retorno listarPacientesEnEspera (int codMedico, LocalDate fecha);    
-    //pre:      post:
+    //pre:      post:Se listan todas las consultas pendientes para el paciente recibido por parametro
     public Retorno consultasPendientesPaciente(int CIPaciente);   
-    //pre:      post:
+    //pre:      post:Se listan todas las consultas a las que el paciente asistio(estado: cerradas)
     public Retorno historiaClínicaPaciente (int ci);    
-    //pre:      post:
+    //pre:      post:Se muestra una matriz con dias (filas), y especialidades (columnas) que muestra cuantos pacientes fueron atendidos por especialidad
     public Retorno reporteDePacientesXFechaYEspecialidad(int mes, int año); 
      
 }
